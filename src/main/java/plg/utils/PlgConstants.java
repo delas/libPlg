65239a224cf6a12f8dc31273b3f8acad3e0bc622
Andrea Burattin <andrea.burattin@gmail.com>
2023-09-26 09:05:39 +0200

    Added automatic determination of library version

--- src/main/java/plg/utils/PlgConstants.java
+++ src/main/java/plg/utils/PlgConstants.java
@@ -1,5 +1,10 @@
 package plg.utils;
 
+import org.apache.maven.model.Model;
+import org.apache.maven.model.io.xpp3.MavenXpp3Reader;
+
+import java.io.FileReader;
+
 /**
  * Constants used by PLG
  * 
@@ -15,11 +20,28 @@ public class PlgConstants {
 	/**
 	 * Current version of libPlg
 	 */
-	public static final String libPLG_VERSION = "2.0.6";
+	public static final String libPLG_VERSION = readVersionFromPomFile();
 	
 	/**
 	 * Signature of the libPlg (typically name and version number)
 	 */
 	public static final String libPLG_SIGNATURE = LibPLG_NAME + " v." + libPLG_VERSION;
+
+	private static String readVersionFromPomFile() {
+		try {
+			// Create a MavenXpp3Reader to parse the pom.xml file
+			MavenXpp3Reader reader = new MavenXpp3Reader();
+			Model model;
+
+			try (FileReader fileReader = new FileReader("pom.xml")) {
+				model = reader.read(fileReader);
+			}
+			return model.getVersion();
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "ERR";
+	}
 }
 
